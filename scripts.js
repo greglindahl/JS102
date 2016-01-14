58217181f1a42a37f1a63e7a1b31ea7dfdeced2b
greglindahl <greglindahl410@gmail.com>
2016-01-13 19:58:27 -0700

    completed objects exercise

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
 //Use this file to implement Part One of your project
 
+var animal = {
+  username: 'yourmom',
+  tagline: ['I love money!'],
+  noises: []
+};
 
+animal.noises.push('woof', 'bark', 'yep');
+
+var counter = 0;
+
+for (var key in animal) {
+  counter ++;
+  console.log(key, counter);
+  if (key === 'username') {
+    console.log('Hi my name is ' + animal.username);
+  } else if (key === 'tagline') {
+    console.log('I like to say ' + animal.tagline);
+  }
+}
+
+console.log(animal);
